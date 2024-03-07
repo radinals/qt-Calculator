e94cf4a78d212c8588e71cbe2f8864154a78a641
radinals <radinalsaragih@gmail.com>
2024-03-07 09:38:44 +0700

    initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,197 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_btn_zero_clicked()
+{
+    this->buffer.append("0");
+    this->out_text.append("0");
+    ui->text_output->setText(out_text);
+}
+
+
+void MainWindow::on_btn_one_clicked()
+{
+    this->buffer.append("1");
+    this->out_text.append("1");
+    ui->text_output->setText(out_text);
+}
+
+
+void MainWindow::on_btn_two_clicked()
+{
+    this->buffer.append("2");
+    this->out_text.append("2");
+    ui->text_output->setText(out_text);
+}
+
+
+void MainWindow::on_btn_three_clicked()
+{
+    this->buffer.append("3");
+    this->out_text.append("3");
+    ui->text_output->setText(out_text);
+}
+
+
+void MainWindow::on_btn_four_clicked()
+{
+    this->buffer.append("4");
+    this->out_text.append("4");
+    ui->text_output->setText(out_text);
+}
+
+
+void MainWindow::on_btn_five_clicked()
+{
+    this->buffer.append("5");
+    this->out_text.append("5");
+    ui->text_output->setText(out_text);
+}
+
+
+void MainWindow::on_btn_six_clicked()
+{
+    this->buffer.append("6");
+    this->out_text.append("6");
+    ui->text_output->setText(out_text);
+}
+
+
+void MainWindow::on_btn_seven_clicked()
+{
+    this->buffer.append("7");
+    this->out_text.append("7");
+    ui->text_output->setText(out_text);
+}
+
+
+void MainWindow::on_btn_eight_clicked()
+{
+    this->buffer.append("8");
+    this->out_text.append("8");
+    ui->text_output->setText(out_text);
+}
+
+
+void MainWindow::on_btn_nine_clicked()
+{
+    this->buffer.append("9");
+    this->out_text.append("9");
+    ui->text_output->setText(out_text);
+}
+
+
+void MainWindow::on_btn_clear_clicked()
+{
+    ui->text_output->clear();
+    this->buffer.clear();
+    this->out_text.clear();
+}
+
+
+void MainWindow::on_btn_div_clicked()
+{
+    if (buffer.isEmpty()) {
+        this->operation.op_one = 0;
+    } else {
+        this->operation.op_one = buffer.toInt();
+    }
+
+    this->operation.op_type = Op_div;
+
+    this->out_text.append(" / ");
+    ui->text_output->setText(out_text);
+    this->buffer.clear();
+}
+
+
+void MainWindow::on_btn_add_clicked()
+{
+    if (buffer.isEmpty()) {
+        this->operation.op_one = 0;
+    } else {
+        this->operation.op_one = buffer.toInt();
+    }
+
+    this->operation.op_type = Op_add;
+
+    this->out_text.append(" + ");
+    ui->text_output->setText(out_text);
+    this->buffer.clear();
+}
+
+
+void MainWindow::on_btn_sub_clicked()
+{
+    if (buffer.isEmpty()) {
+        this->operation.op_one = 0;
+    } else {
+        this->operation.op_one = buffer.toInt();
+    }
+
+    this->operation.op_type = Op_sub;
+
+    this->out_text.append(" - ");
+    ui->text_output->setText(out_text);
+    this->buffer.clear();
+}
+
+
+void MainWindow::on_btn_mul_clicked()
+{
+    if (buffer.isEmpty()) {
+        this->operation.op_one = 0;
+    } else {
+        this->operation.op_one = buffer.toInt();
+    }
+
+    this->operation.op_type = Op_mul;
+    this->buffer.clear();
+
+    this->out_text.append(" X ");
+    ui->text_output->setText(out_text);
+    this->buffer.clear();
+}
+
+
+void MainWindow::on_btn_equal_clicked()
+{
+    if (buffer.isEmpty()) {
+        this->operation.op_two = 0;
+    } else {
+        this->operation.op_two = buffer.toInt();
+    }
+
+    ui->text_output->setText(QString::number(get_result()));
+    this->out_text.clear();
+}
+
+int MainWindow::get_result() {
+    switch(this->operation.op_type) {
+    case Op_add:
+        return this->operation.op_one + this->operation.op_two;
+    case Op_sub:
+        return this->operation.op_one - this->operation.op_two;
+    case Op_mul:
+        return this->operation.op_one * this->operation.op_two;
+    case Op_div:
+        if (operation.op_one == 0 || operation.op_two == 0)
+            return 0;
+        else
+            return this->operation.op_one / this->operation.op_two;
+    }
+
+    return 0;
+}
---
+++ mainwindow.h
@@ -0,0 +1,77 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+private:
+    enum CalcOperator{
+       Op_add,
+       Op_sub,
+       Op_mul,
+       Op_div,
+    };
+
+    struct CalcInfo {
+        CalcOperator op_type;
+        int op_one;
+        int op_two;
+    };
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_btn_zero_clicked();
+
+    void on_btn_one_clicked();
+
+    void on_btn_two_clicked();
+
+    void on_btn_three_clicked();
+
+    void on_btn_four_clicked();
+
+    void on_btn_five_clicked();
+
+    void on_btn_six_clicked();
+
+    void on_btn_seven_clicked();
+
+    void on_btn_eight_clicked();
+
+    void on_btn_nine_clicked();
+
+    void on_btn_clear_clicked();
+
+    void on_btn_div_clicked();
+
+    void on_btn_add_clicked();
+
+    void on_btn_sub_clicked();
+
+    void on_btn_mul_clicked();
+
+    void on_btn_equal_clicked();
+
+private:
+    Ui::MainWindow *ui;
+
+private:
+    QString buffer;
+    QString out_text;
+    struct CalcInfo operation;
+
+    int get_result();
+};
+#endif // MAINWINDOW_H
